3e2308cb3203ebf250157e8f933d331e6f385032
Carlos Henrique <carlos.farias.pb@compasso.com.br>
2022-03-23 07:23:56 -0300

    adicionado a função de imagem de perfil

--- src/components/ListMovies/index.js
+++ src/components/ListMovies/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import MovieList from '../../screens/MovieList';
-import { Container, Title, SubTitle, TitleList, Name } from './styles';
-import { Button } from 'react-native';
+import { Container, Title, SubTitle, TitleList, Name, Avatar } from './styles';
+import { Button,Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StyleSheet } from 'react-native';
 
 function ListMovies({ navigation }) {
   const [name, setName] = useState('');
@@ -32,6 +33,11 @@ function ListMovies({ navigation }) {
     <>
       <Button title='sair' onPress={removeAccountData} />
       <Container>
+      <Image style = {Perfil.imagem}
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w300/${avatar}`,
+                }}
+              />
         <Title>
           Olá, <Name>{name === null ? username : name}</Name>!
         </Title>
@@ -43,5 +49,14 @@ function ListMovies({ navigation }) {
     </>
   );
 }
+const Perfil = StyleSheet.create({
+imagem: {
+width: 44,
+height: 44,
+top: 18,
+left:312,
+borderRadius:40
+  },
+})
 
 export default ListMovies;
