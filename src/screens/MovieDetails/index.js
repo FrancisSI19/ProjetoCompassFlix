8a7229e230244d5a58d4126283b8654060eae18a
Vitor Gomes <vitor.gomes.pb@compasso.com.br>
2022-03-23 10:44:56 -0300

    Ajustar posicionamento do titulo, ano de lancamento e tempo de duracao do filme. Adicionar botoes de favoritar e avaliar filme.

--- src/screens/MovieDetails/index.js
+++ src/screens/MovieDetails/index.js
@@ -1,16 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Image, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import styles from './styles';
+
 import {fetchCredits, fetchDetails} from '../../services/api';
 import Loading from '../../components/Loading';
-import {
-  Container, ContainerDetails, IconBack, MovieBackground, MovieSection,
-  Image, MovieInformationContainer, MovieInformation, Title,
-  ContainerText, MovieYear, MovieDuration, Direction,
-  ContainerRating, VoteAverage, ContainerVote, VoteCount,
-  Overview, CastTag, Cast, Border, CastContainer, CastImage,
-  ContainerCastInfo, NameCast, NameCharacter, Director
-} from './styles';
-import { ScrollView } from 'react-native';
 
   const MovieDetails = ({ navigation, route }) => {
   const [credits, setCredits] = useState(null);
@@ -28,12 +24,11 @@ import { ScrollView } from 'react-native';
   const [cast, setCast] = useState([]);
 
   const {movieId} = route.params;
-  
 
   useEffect(() => {
-    setLoading(true);
-
     fetchDetails(movieId).then((data) => {
+      setLoading(true);
+
       setBackdrop(data.backdrop);
       setPoster(data.poster);
       setTitle(data.title);
@@ -42,86 +37,130 @@ import { ScrollView } from 'react-native';
       setVoteAverage(data.voteAverage);
       setVoteCount(data.voteCount);
       setOverview(data.overview);
-    })
+
+      setLoading(false);
+    });
 
     fetchCredits(movieId).then((data) => {
+      setLoading(true);
+
       setCredits(data.credits);
       setDirector(data.director);
       setCast(data.cast);
-    });
 
-    setLoading(false);
+      setLoading(false);
+    });
   }, []);
 
-  return loading ? <Loading /> : (
-    <Container>
+  return loading ? <Loading size={60} /> : (
+    <View style={styles.root}>
       <ScrollView>
-        <MovieBackground
+        <Image
+          style={styles.backdrop}
           source={{ uri: `https://image.tmdb.org/t/p/w780${backdrop}` }}
         />
 
-        <IconBack onPress={() => navigation.navigate('ListMovies')}>
-          <Icon name='arrow-back' size={24} color='#000' />
-        </IconBack>
+        <TouchableOpacity
+          style={styles.btnBack}
+          onPress={() => navigation.navigate('ListMovies')}
+        >
+          <Ionicons name='arrow-back' size={26} color='#000' />
+        </TouchableOpacity>
 
-        <ContainerDetails>
-          <MovieSection>
+        <TouchableOpacity
+          style={styles.btnFavorite}
+        >
+          <MaterialIcons name='star-border' size={26} color='#000' />
+          {/* <MaterialIcons name='star' size={26} color='#EC2626' /> */}
+        </TouchableOpacity>
+
+        <View style={styles.mainSection}>
+          <View style={styles.poster}>
             <Image
-              source={{ uri: `https://image.tmdb.org/t/p/w780${poster}` }} 
+              style={{ width: 116, height: 172}}
+              source={{ uri: `https://image.tmdb.org/t/p/w780${poster}` }}
             />
 
-            <MovieInformationContainer>
-              <MovieInformation>
-                <Title>{title}</Title>
-                <ContainerText>
-                  <MovieYear>{releaseYear}</MovieYear>
-                  <MovieDuration>{runtime} min</MovieDuration>
-                </ContainerText>
-              </MovieInformation>
+            <TouchableOpacity
+              style={{
+                alignItems: 'center',
 
-              <Direction>
-                Direção por 
-                <Director> {director?.name}</Director>
-              </Direction>
+                padding: 4,
+                borderRadius: 5,
+                backgroundColor: '#E9A6A6'
+              }}
+            >
+              <Text style={{ color: '#000', textTransform: 'uppercase', fontSize: 10, fontFamily: 'OpenSans-Bold' }}>Avalie agora</Text>
+            </TouchableOpacity>
+          </View>
 
-              <ContainerRating>
-                <VoteAverage>{voteAverage}/10</VoteAverage>
+          <View style={[styles.mediaInfoEnvelope]}>
+            <Text style={styles.title}>
+              {title}
+            </Text>
+            <View style={styles.timeInfoEnvelope}>
+              <Text style={styles.releaseYear}>{releaseYear} | </Text>
+              <Text style={styles.runtime}>{runtime} min</Text>
+            </View>
 
-                <ContainerVote>
-                  <Icon name='heart' size={24} color='#EC2626' />
-                  <VoteCount>{voteCount >= 1000 ? `${(voteCount/1000).toFixed(0)}k` : voteCount}</VoteCount>
-                </ContainerVote>
-              </ContainerRating>
-            </MovieInformationContainer>
-          </MovieSection>
+            <Text style={styles.directionText}>
+              Direção por
+              <Text style={styles.directorName}> {director?.name} </Text>
+            </Text>
 
-          <Overview>
+            <View style={styles.ratingEnvelope}>
+              <Text style={styles.voteAverage}>
+                {voteAverage}/10
+              </Text>
+
+              <View style={styles.voteCountEnvelope}>
+                <Ionicons name='heart' size={24} color='#EC2626' />
+                <Text style={styles.voteCount}>
+                  {
+                    voteCount >= 1000
+                    ? `${(voteCount/1000).toFixed(0)}k`
+                    : voteCount
+                  }
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <Text style={styles.overview}>
             {overview}
-          </Overview>
-          
-          <CastTag>
-            <Cast>Elenco</Cast>
-            <Border/>
-          </CastTag>
-          
+          </Text>
+
+          <View style={styles.castTag}>
+            <Text style={styles.castText}>Elenco</Text>
+            <View style={styles.castBorder} />
+          </View>
+
           {
             cast.map(item => {
               return (
-                <CastContainer key={item.id}>
-                  <CastImage
-                    source={{ uri: `https://image.tmdb.org/t/p/w780${item.profilePath}` }} 
+                <View
+                  style={styles.castEnvelope}
+                  key={item.id}
+                >
+                  <Image
+                    style={styles.castProfile}
+                    source={{ uri: `https://image.tmdb.org/t/p/w780${item.profilePath}` }}
                   />
-                  <ContainerCastInfo>
-                    <NameCast>{item.originalName}</NameCast>
-                    <NameCharacter>{item.characterName}</NameCharacter>
-                  </ContainerCastInfo>
-                </CastContainer>
+                  <View style={styles.castInfoEnvelope}>
+                    <Text style={styles.castName}>
+                      {item.originalName}
+                    </Text>
+                    <Text style={styles.characterName}>
+                      {item.characterName}
+                    </Text>
+                  </View>
+                </View>
               );
             })
           }
-          </ContainerDetails>
-        </ScrollView>
-    </Container>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
--- src/screens/MovieDetails/styles.js
+++ src/screens/MovieDetails/styles.js
@@ -1,165 +1,152 @@
-import styled from "styled-components/native";
+import { StyleSheet } from 'react-native';
 
-export const Container = styled.View`
-   background-color: #000;
-    
-`;
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: '#000'
+  },
+  backdrop: {
+    width: '100%',
+    height: 160
+  },
+  btnBack: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 30,
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    backgroundColor: '#fff'
+  },
+  btnFavorite: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 30,
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: '#fff'
+  },
+  mainSection: {
+    padding: 20
+  },
+  poster: {
+    width: 116,
+    height: 182,
+    position: 'absolute',
+    top: -40,
+    left: 20,
+    borderRadius: 6
+  },
+  mediaInfoEnvelope: {
+    width: '64%',
+    position: 'absolute',
+    top: 4,
+    right: 20,
+  },
+  mainMediaInfo: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'baseline'
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: 'OpenSans-Bold',
+    color: '#fff'
+  },
+  timeInfoEnvelope: {
+    marginTop: 2,
+    flexDirection: 'row',
+    alignItems: 'baseline'
+  },
+  releaseYear: {
+    fontSize: 12,
+    fontFamily: 'OpenSans-Regular',
+    color: '#fff'
+  },
+  runtime: {
+    fontSize: 10,
+    fontFamily: 'OpenSans-Regular',
+    color: '#fff'
+  },
+  directionText: {
+    fontSize: 10,
+    fontFamily: 'OpenSans-Regular',
+    color: '#fff'
+  },
+  directorName: {
+    fontFamily: 'OpenSans-Bold'
+  },
+  ratingEnvelope: {
+    marginTop: 6,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  voteAverage: {
+    fontSize: 30,
+    color: '#E9A6A6'
+  },
+  voteCountEnvelope: {
+    alignItems: 'center',
+    marginLeft: 40
+  },
+  voteCount: {
+    fontSize: 12,
+    fontFamily: 'OpenSans-Regular',
+    color: '#fff'
+  },
+  overview: {
+    marginTop: 160,
+    fontFamily: 'OpenSans-Regular',
+    color: '#fff'
+  },
+  castTag: {
+    alignItems: 'center',
+    alignSelf: 'flex-start',
+    marginTop: 24
+  },
+  castText: {
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+    borderRadius: 50,
+    fontSize: 12,
+    fontFamily: 'OpenSans-SemiBold',
+    backgroundColor: '#9C4A8B',
+    color: '#fff'
+  },
+  castBorder: {
+    width: 28,
+    marginTop: 4,
+    borderWidth: 0.8,
+    borderRadius: 10,
+    borderColor: '#9C4A8B'
+  },
+  castEnvelope: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  castProfile: {
+    width: 46,
+    height: 46,
+    borderRadius: 30
+  },
+  castInfoEnvelope: {
+    marginLeft: 14
+  },
+  castName: {
+    fontSize: 16,
+    fontFamily: 'OpenSans-Bold',
+    color: '#fff'
+  },
+  characterName: {
+    fontSize: 10,
+    fontFamily: 'OpenSans-Regular',
+    color: '#fff'
+  }
+});
 
-export const MovieBackground = styled.Image`
-    width: 100%;
-    height: 160px;
-`;
-
-export const IconBack = styled.TouchableOpacity`
-    align-items: center;
-    justify-content: center;
-    width: 30px;
-    height: 30px;
-    border-radius: 30px;
-    position: absolute;
-    top: 20px;
-    left: 20px;
-    background-color: #fff;
-
-`;
-export const ContainerDetails = styled.View`
-    padding: 20px;
-`;
-
-export const MovieSection = styled.View`
-    flex-direction: row;
-    align-items: flex-end;
-    position: relative;
-    top: -58px;
-`
-
-export const Image = styled.Image`
-    margin-top: 15px;
-    width: 116px;
-    height: 182px;
-    border-radius: 6px;
-    
-`;
-
-export const MovieInformationContainer = styled.View`
-    flex: 1;
-    flex-wrap: wrap;
-    margin-left: 20px;
-`;
-
-export const MovieInformation = styled.View`
-    flex-direction: row;
-    align-items: baseline;
-    flex-wrap: wrap;
-`;
-
-export const Title = styled.Text`
-    margin-right: 10px;
-    font-size: 22px;
-    font-family: OpenSans-Bold;
-    color: #fff;
-`;
-
-export const ContainerText = styled.View`
-    flex-direction: row;
-    align-items: center;
-`;
-
-export const MovieYear = styled.Text`
-    font-size: 12px;
-    font-family: OpenSans-Regular; 
-    color: #fff;
-`;
-
-export const MovieDuration = styled.Text`
-    margin-left: 20px;
-    font-size: 10px;
-    font-family: OpenSans-Regular;
-    color: #fff;
-`;
-
-export const Direction = styled.Text`
-    font-size: 10px; 
-    font-family: OpenSans-Regular;
-    color: #fff;
-`;
-export const Director = styled.Text`
-    font-family: OpenSans-Bold;
-`;
-
-export const ContainerRating = styled.View`
-    flex-direction: row;
-    align-items: center;
-    margin-top: 10px;
-`;
-export const VoteAverage = styled.Text`
-    font-size: 30px; 
-    color: #E9A6A6;
-`;
-
-export const ContainerVote = styled.View`
-    align-items: center;
-    margin-left: 50px;
-`;
-
-export const VoteCount = styled.Text`
-    font-size: 12px; 
-    font-family: OpenSans-Regular;
-    color: #fff;
-`;
-
-export const Overview = styled.Text`
-    margin-top: -22px;
-    font-family: OpenSans-Regular;
-    color: #fff;
-`;
-
-export const CastTag = styled.View`
-    align-items: center;
-    align-self: flex-start;
-    margin-top: 24px;
-`;
-
-export const Cast = styled.Text`
-    padding-vertical: 2px;
-    padding-horizontal: 8px;
-    border-radius: 50px;
-    font-size: 12px;
-    font-family: OpenSans-SemiBold;
-    background-color: #9C4A8B;
-    color: #fff;
-`;
-
-export const Border = styled.View`
-    width: 28px;
-    margin-top: 4px; 
-    border-width: 0.8px;
-    border-radius: 10px;
-    border-color: #9C4A8B;
-`;
-
-export const CastContainer = styled.View`
-    flex-direction: row;
-    align-items: center; 
-    margin-top: 20px
-`;
-
-export const CastImage = styled.Image`
-    width: 46px;
-    height: 46px;
-    border-radius: 30px;
-`;
-export const ContainerCastInfo = styled.View`
-    margin-left: 14px;
-`;
-
-export const NameCast = styled.Text`
-    font-size: 16px; 
-    font-family: OpenSans-Bold;
-    color: #fff;
-`;
-export const NameCharacter = styled.Text`
-    font-size: 10px;
-    font-family: OpenSans-Regular;
-    color: #fff;
-`;
+export default styles;
