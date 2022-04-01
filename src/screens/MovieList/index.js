94b15727658beaf6333d654d539b50d3deccb652
FrancisSI19 <tauan_19@hotmail.com>
2022-04-01 14:02:57 -0300

    Atualizando devel com loading

--- src/screens/MovieList/index.js
+++ src/screens/MovieList/index.js
@@ -1,11 +1,10 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from 'react';
-import { FlatList, TouchableOpacity } from 'react-native';
+import { FlatList, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { ContainerVote, Vote, Image, Container } from '../styles';
 import {fetchMovies} from "../../services/api";
-import Loading from '../../components/Loading';
 import IconStar from '../../components/IconStar';
 import VoteAverage from '../../components/VoteAverage';
 
@@ -28,29 +27,50 @@ export default function MovieList() {
 
   const loadMoreItem = () => setPageNumber(pageNumber + 1);
 
-  return loading ? (<Loading />) : (
-    <FlatList
-      data={movies}
-      numColumns={4}
-      keyExtractor={( item, index) => String(index)}
-      onEndReached={loadMoreItem}
-      onEndReachedThreshold={0.5}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item }) => {
-        return (
-          <Container>
-            <TouchableOpacity onPress={ () => navigation.navigate('MovieDetails', {movieId: item.id, requestScreen: 'ListMovies'})}>
-              <Image source={{uri: `https://image.tmdb.org/t/p/w780${item.poster_path}`}}/>
-            </TouchableOpacity>
-            <ContainerVote>
-              <IconStar/>
-              <Vote>
-                {item.vote_average}<VoteAverage/>
-              </Vote>
-            </ContainerVote>
-          </Container>
-        );
-      }}
-    />
-  );
-}
+  function FooterList({ load }) {
+    if (!load) return null;
+    return (
+      <View style={{ padding: 10 }}>
+        <ActivityIndicator size={'large'} color='#E9A6A6' />
+      </View>
+    )
+  }
+    return (
+      <FlatList
+        data={movies}
+        numColumns={4}
+        keyExtractor={(item, index) => String(index)}
+        onEndReached={loadMoreItem}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={<FooterList load={loading} />}
+        showsVerticalScrollIndicator={false}
+        renderItem={renderItem}
+      />
+    )
+    function renderItem({ item }) {
+      return (
+        <Container>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('MovieDetails', {
+                movieId: item.id,
+                requestScreen: 'ListMovies',
+              })
+            }>
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w780${item.poster_path}`,
+              }}
+            />
+          </TouchableOpacity>
+          <ContainerVote>
+            <IconStar />
+            <Vote>
+              {item.vote_average}
+              <VoteAverage />
+            </Vote>
+          </ContainerVote>
+        </Container>
+      );
+    }
+  }
--- src/screens/TVShowsList/index.js
+++ src/screens/TVShowsList/index.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, TouchableOpacity} from 'react-native';
+import {FlatList, TouchableOpacity, View, ActivityIndicator} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import {ContainerVote, Vote, Image, Container} from '../styles';
 import {fetchTVShow} from "../../services/api";
-import Loading from '../../components/Loading';
 import IconStar from '../../components/IconStar';
 import VoteAverage from '../../components/VoteAverage';
 
@@ -27,29 +26,39 @@ export default function TVShowsList() {
 
   const loadMoreItem = () => setPageNumber(pageNumber + 1);
 
-  return loading ? (<Loading />) : (
+  function FooterList({ load }) {
+    if (!load) return null;
+    return (
+      <View style={{ padding: 10 }}>
+        <ActivityIndicator size={'large'} color='#E9A6A6' />
+      </View>
+    )
+  }
+  return (
     <FlatList
       data={tvshows}
       numColumns={4}
-      keyExtractor={( item, index) => String(index)}
+      keyExtractor={(item, index) => String(index)}
       onEndReached={loadMoreItem}
-      onEndReachedThreshold={0.5}
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={<FooterList load={loading} />}
       showsVerticalScrollIndicator={false}
-      renderItem={({ item }) => {
-        return (
-          <Container>
-            <TouchableOpacity onPress={ () => navigation.navigate('TvShowDetails', {tvShowId: item.id, requestScreen: 'TVShowList'})}>
-              <Image source={{uri: `https://image.tmdb.org/t/p/w780${item.poster_path}`}}/>
-            </TouchableOpacity>
-            <ContainerVote>
-              <IconStar/>
-              <Vote>
-                {item.vote_average}<VoteAverage/>
-              </Vote>
-            </ContainerVote>
-          </Container>
-        );
-      }}
+      renderItem={renderItem}
     />
   );
+  function renderItem({ item }) {
+    return (
+      <Container>
+        <TouchableOpacity onPress={() => navigation.navigate('', { tvshowId: item.id })}>
+          <Image source={{ uri: `https://image.tmdb.org/t/p/w780${item.poster_path}` }} />
+        </TouchableOpacity>
+        <ContainerVote>
+          <IconStar />
+          <Vote>
+            {item.vote_average}<VoteAverage />
+          </Vote>
+        </ContainerVote>
+      </Container>
+    );
+  }
 }
