a351c41400842631304cd467dce8a1167e223a0d
Carlos Henrique <carlos.farias.pb@compasso.com.br>
2022-03-31 22:43:45 -0300

    1

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -118,6 +118,10 @@ function ProfileStack() {
         name="RatedMovies"
         component={RatedMovies}
       />
+      <Stack.Screen
+        name="RatedSeries"
+        component={RatedSeries}
+      />
 
       <Stack.Screen
         name="FavoritesSeries"
