b275ad1a98a4cdcba26b9aa56850e4cc369d30d4
FrancisSI19 <tauan_19@hotmail.com>
2022-04-14 13:12:11 -0300

    Desativando/ativando botão de Ok em avaliação das séries

--- src/components/Favorites/FavoritesSeries/index.js
+++ src/components/Favorites/FavoritesSeries/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Name, IconBack, Title, ContainerFavorite} from './styles';
+import { Container, Name, IconBack, Title, ContainerFavorite} from '../styles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import SeriesFavorites from '../../screens/SeriesFavorites';
+import SeriesFavorites from '../../../screens/SeriesFavorites';
 import { useNavigation } from '@react-navigation/native';
-//{ route: {params} , navigation } inserir na função da tela do usuário quando tiver a tela do retorno.
+
 function FavoriteSeries() {
   const navigation = useNavigation();
   const [name, setName] = useState('');
